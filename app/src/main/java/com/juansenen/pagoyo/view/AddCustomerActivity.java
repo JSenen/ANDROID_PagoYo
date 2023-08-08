4e34ed23a21e81fbcb678635d244ddf40eb0e6ef
Juan Senen <juansenen@hotmail.com>
2023-08-08 10:54:31 +0200

    Start Add customer activity

---
+++ app/src/main/java/com/juansenen/pagoyo/view/AddCustomerActivity.java
@@ -0,0 +1,14 @@
+package com.juansenen.pagoyo.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.juansenen.pagoyo.R;
+
+public class AddCustomerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_customer);
+    }
+}
